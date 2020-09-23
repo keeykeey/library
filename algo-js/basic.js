3fc8ae686b5ab14129d9e9b8e58f49db56eb0296
keeykeey <sh1n_fuj1to@icloud.com>
2020-09-24 00:54:31 +0900

    function to get L1 norm

--- algo-js/basic.js
+++ algo-js/basic.js
@@ -1,13 +1,23 @@
-function getDistance(arr1,arr2){
-    let loss = 0;
-    for (let integer = 0; integer < arr1.length; integer ++){
-        loss += (arr1[integer]-arr2[integer])
+function getSumOfLoss(_y,_x){
+    //_y is a array of test data and _x is a array of train data
+    const loss = 0;
+    for (let integer = 0; integer < _y.length; integer ++){
+        loss += (_y[integer]-_x[integer])
     } 
     return loss
 }
 
+function get_L1_norm(x1,y1,x2,y2){
+    const d1 = Math.abs(x2 - x1)
+    const d2 = Math.abs(y2 - y1)
+    const distance = Math.abs(d1 + d2)
+
+    return distanve
+} 
+    
 arr1 = [10,10,10]
 arr2 = [5,5,5]
 loss = getDistance(arr1,arr2);
 console.log(loss);
+
 
