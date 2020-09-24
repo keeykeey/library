d3e53f5f30aeeffceaf2b65b74c17faf3b2e6498
keeykeey <sh1n_fuj1to@icloud.com>
2020-09-25 00:41:17 +0900

    function to get L2 norm

--- algo-js/basic.js
+++ algo-js/basic.js
@@ -4,19 +4,29 @@ function getSumOfLoss(_y,_x){
     for (let integer = 0; integer < _y.length; integer ++){
         loss += (_y[integer]-_x[integer])
     } 
-    return loss
+    return loss;
 }
 
 function get_L1_norm(x1,y1,x2,y2){
-    const d1 = Math.abs(x2 - x1)
-    const d2 = Math.abs(y2 - y1)
-    const distance = Math.abs(d1 + d2)
+    const d1 = Math.abs(x2 - x1);
+    const d2 = Math.abs(y2 - y1);
+    const distance = Math.abs(d1 + d2);
 
-    return distanve
+    return distanve;
 } 
     
-arr1 = [10,10,10]
-arr2 = [5,5,5]
+function get_L2_norm(x1,y1,x2,y2){
+    const d1 = Math.abs(x2 - x1);
+    const d2 = Math.abs(y2 - y1);
+    const distance = Math.abs((d1+d2)**2);
+
+    return distanve;
+} 
+
+
+
+arr1 = [10,10,10];
+arr2 = [5,5,5];
 loss = getDistance(arr1,arr2);
 console.log(loss);
 
