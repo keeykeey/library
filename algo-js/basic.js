af648bcc9cb007cc9f3819395413c714219c434a
keeykeey <sh1n_fuj1to@icloud.com>
2020-12-30 11:11:32 +0900

    corrected typo

--- algo-js/basic.js
+++ algo-js/basic.js
@@ -12,7 +12,7 @@ function get_L1_norm(x1,y1,x2,y2){
     const d2 = Math.abs(y2 - y1);
     const distance = Math.abs(d1 + d2);
 
-    return distanve;
+    return distance;
 } 
     
 function get_L2_norm(x1,y1,x2,y2){
@@ -20,14 +20,6 @@ function get_L2_norm(x1,y1,x2,y2){
     const d2 = Math.abs(y2 - y1);
     const distance = Math.abs((d1+d2)**2);
 
-    return distanve;
+    return distance;
 } 
-
-
-
-arr1 = [10,10,10];
-arr2 = [5,5,5];
-loss = getDistance(arr1,arr2);
-console.log(loss);
-
 
